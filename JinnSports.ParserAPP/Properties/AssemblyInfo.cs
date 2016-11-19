b938af81db66ea0687c3779c865311b8f9bfccbf
Denis Polozhyi <my_email@whatever.com>
2016-11-19 16:14:08 +0300

    Changed ParserProject name to JinnSports.ParserAPP

--- JinnSports.ParserAPP/Properties/AssemblyInfo.cs
+++ JinnSports.ParserAPP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JinnSportsParser.APP")]
+[assembly: AssemblyTitle("JinnSports.ParserAPP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("JinnSportsParser.APP")]
+[assembly: AssemblyProduct("JinnSports.ParserAPP")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
