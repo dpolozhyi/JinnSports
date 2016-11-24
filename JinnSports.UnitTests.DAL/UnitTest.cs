ec8a642e1e16ae0058def3e2ddf93fa16903714c
YatsenkoOleksandr <yats.sasha@gmail.com>
2016-11-24 21:52:43 +0200

    Created unit tests projects

---
+++ JinnSports.UnitTests.DAL/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JinnSports.UnitTests.DAL
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(31, 17);
+        }
+    }
+}
