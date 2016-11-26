30f4ed85fbf3a3aa3b9d282b5babf91e43dca727
ivan-rataichuk <ivan.rataichuk@gmail.com>
2016-11-26 17:44:11 +0200

    DAL changes
    Made changes to DA Intefaces, UoW and BaseRepository classes.

--- JinnSports.DAL/Repositories/EFUnitOfWork.cs
+++ JinnSports.DAL/Repositories/EFUnitOfWork.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 using JinnSports.DataAccessInterfaces;
 using JinnSports.DAL.EF;
+using JinnSports.DAL.Entities;
 
 namespace JinnSports.DAL.Repositories
 {
-    public class EFUnitOfWork<T> : IUnitOfWork<T> where T : class
+    public class EFUnitOfWork : IUnitOfWork
     {
         private SportsContext db;
-        private BaseRepository<T> baseRepository;
-       
+        private IRepository<Team> Teams;
+        private IRepository<Result> Results;
+        private IRepository<CompetitionEvent> CompetitionEvents;
+        private IRepository<SportType> SportTypes;
+
         public EFUnitOfWork()
         {
             db = new SportsContext();
+            Teams = new BaseRepository<Team>(db.Teams);
+            Results = new BaseRepository<Result>(db.Results);
+            CompetitionEvents = new BaseRepository<CompetitionEvent>(db.CompetitionEvents);
+            SportTypes = new BaseRepository<SportType>(db.SportTypes);
         }
 
-        public IRepository<T> GetRepository()
-        {
-            if (baseRepository == null)
-            {
-                baseRepository = new BaseRepository<T>(db);
-            }
-            return baseRepository;
-        }
-        public void Save()
+        public void SaveChanges()
         {
             db.SaveChanges();
         }
