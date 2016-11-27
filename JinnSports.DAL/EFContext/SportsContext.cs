a8b8983a2b22764275ffc7038afa9b6412bc2796
ivan-rataichuk <ivan.rataichuk@gmail.com>
2016-11-27 17:00:39 +0200

    SportContext changes
    Made changes in SportContext.cs and App.config

--- JinnSports.DAL/EFContext/SportsContext.cs
+++ JinnSports.DAL/EFContext/SportsContext.cs
@@ -11,27 +11,10 @@ namespace JinnSports.DAL.EF
         public DbSet<Result> Results;
         public DbSet<SportType> SportTypes;
         public DbSet<Team> Teams;
-
-        static SportsContext()
+       
+        SportsContext() : base("SqlServerConnection")
         {
             AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
-            Database.SetInitializer<SportsContext>(new StoreDbInitializer());
-        }
-       
-        public class StoreDbInitializer : DropCreateDatabaseIfModelChanges<SportsContext>
-        {
-            protected override void Seed(SportsContext db)
-            {
-                db.Teams.Add(new Team()
-                {
-                    Name = "MC"
-                });
-                db.Teams.Add(new Team()
-                {
-                    Name = "MU"
-                });
-                db.SaveChanges();
-            }
         }
     }
 }
