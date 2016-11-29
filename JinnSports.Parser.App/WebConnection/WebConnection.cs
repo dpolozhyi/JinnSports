7accfaee97190063fc29ad329fda8d530971bede
ivan-rataichuk <ivan.rataichuk@gmail.com>
2016-11-30 00:31:41 +0200

    WebConnections
    Added classess WebConnection and WebProxyConnection

---
+++ JinnSports.Parser.App/WebConnection/WebConnection.cs
@@ -0,0 +1,38 @@
+﻿using JinnSports.Parser.App.Inerfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace JinnSports.Parser.App.WebConnection
+{
+    class WebConnection : IWebConnection
+    {
+        private string url;
+        private string encoding = "utf-8";
+
+        public WebConnection()
+        {
+
+        }
+
+        public WebResponse GetResponse()
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Headers.Set(HttpRequestHeader.ContentEncoding, encoding);
+            return request.GetResponse();
+        }
+
+        public void SetHeaderEncoding(string encoding)
+        {
+            this.encoding = encoding;
+        }
+
+        public void SetURL(string url)
+        {
+            this.url = url;
+        }
+    }
+}
