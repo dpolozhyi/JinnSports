97f66f6cd25cb2398006327e187b544ad1406799
mpdelly <belotserkovski001@yandex.ru>
2016-12-01 22:28:29 +0200

    JSON parser + Proxy Parser + Architecture rework

--- JinnSports.Parser.App/Program.cs
+++ JinnSports.Parser.App/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using JinnSports.DAL.Repositories;
 using JinnSports.DAL.Entities;
 using System.Data.SqlClient;
-using JinnSports.Parser.App.JsonEntities;
+using JinnSports.Parser.App.JsonParserService;
+using JinnSports.Parser.App.ProxyService.ProxyParser;
+using JinnSports.Parser.App.ProxyService.ProxyConnection;
 
 namespace JinnSports.Parser.App
 {
@@ -12,7 +14,13 @@ namespace JinnSports.Parser.App
     {
         static void Main(string[] args)
         {
-            
+           // ProxyParser pp = new ProxyParser();
+            //pp.UpdateData(true);
+            //pp.UpdateData();
+            //ProxyConnection pc = new ProxyConnection();
+            //string proxy = pc.GetProxy();
+            //pc.SetStatus(proxy, true);
+            string a = ConfigSection.XmlPath();
         }
     }
 }
