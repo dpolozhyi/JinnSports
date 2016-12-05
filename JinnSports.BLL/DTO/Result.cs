552a6edc21b8ea05e69ecd407bd721826c3dc1d9
Nikita Magda <nikmagdaa@gmail.com>
2016-12-05 15:42:21 +0200

    Recreated WEB project

---
+++ JinnSports.BLL/DTO/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JinnSports.BLL.DTO
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public virtual Team Team { get; set; }
+        public virtual CompetitionEvent CompetitionEvent { get; set; }
+        public string Score { get; set; }
+    }
+}
