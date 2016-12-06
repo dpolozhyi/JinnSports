7602b3106ad5ffbaf961ec232223a38479fb09c5
YatsenkoOleksandr <yats.sasha@gmail.com>
2016-12-06 16:45:08 +0200

    Created Controller. Created event outputing.

---
+++ JinnSports.WEB/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using JinnSports.BLL.DTO;
+using JinnSports.BLL.Interfaces;
+using JinnSports.BLL.Service;
+using JinnSports.WEB.Mapper;
+using JinnSports.WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JinnSports.WEB.Controllers
+{
+    public class EventController:Controller
+    {
+        public ActionResult History()
+        {
+            IEventService bllService = new EventsService();
+            IList<CompetitionEventDTO> events = bllService.GetCEvents();
+            IList<SportResultsViewModel> viewModel = EventSorter.Sort(events);
+            bllService.Dispose();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ JinnSports.WEB/Models/ResultViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JinnSports.WEB.Models
+{
+    public class ResultViewModel
+    {
+        public string Result
+        {
+            get; set;
+        }
+
+        public string Date
+        {
+            get; set;
+        }
+    }
+}
