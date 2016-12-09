02da0414f5ee17c1005bb5bee067d369b8151ffe
Nikita Magda <nikmagdaa@gmail.com>
2016-12-09 21:23:38 +0200

    Fixed merge issues

--- JinnSports.Parser.App/Program.cs
+++ JinnSports.Parser.App/Program.cs
@@ -9,11 +9,8 @@ using JinnSports.Parser.App.JsonParsers;
 using JinnSports.Parser.App.ProxyService.ProxyParser;
 using JinnSports.Parser.App.ProxyService.ProxyConnection;
 using JinnSports.DataAccessInterfaces.Interfaces;
-<<<<<<< HEAD
 using JinnSports.Entities.Entities;
-=======
 using System.Net;
->>>>>>> 806d646c7cf07f30d62598c7179ee887f3fcdeff
 
 namespace JinnSports.Parser.App
 {
