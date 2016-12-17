5f758b6a792301a57d878902854ec05ad71719af
ivan-rataichuk <ivan.rataichuk@gmail.com>
2016-12-17 14:29:26 +0200

    StyleCop and deploy.proj issues resolved

--- JinnSports.UnitTests.Entities/SportEventTest.cs
+++ JinnSports.UnitTests.Entities/SportEventTest.cs
@@ -15,42 +15,42 @@ namespace JinnSports.UnitTests.Entities
         [SetUp]
         public void Init()
         {
-            sportType = new SportType { Id = 0, Name = "Football" };
+            this.sportType = new SportType { Id = 0, Name = "Football" };
 
-            Team team1 = new Team { Id = 0, Name = "Team_1", SportType = sportType };
-            Team team2 = new Team { Id = 1, Name = "Team_2", SportType = sportType };
-            Team team3 = new Team { Id = 2, Name = "Team_3", SportType = sportType };
+            Team team1 = new Team { Id = 0, Name = "Team_1", SportType = this.sportType };
+            Team team2 = new Team { Id = 1, Name = "Team_2", SportType = this.sportType };
+            Team team3 = new Team { Id = 2, Name = "Team_3", SportType = this.sportType };
 
-            res1 = new Result { Id = 0, Team = team1, Score = 3 };
-            res2 = new Result { Id = 1, Team = team2, Score = 1 };
-            res3 = new Result { Id = 2, Team = team3, Score = 5 };
+            this.res1 = new Result { Id = 0, Team = team1, Score = 3 };
+            this.res2 = new Result { Id = 1, Team = team2, Score = 1 };
+            this.res3 = new Result { Id = 2, Team = team3, Score = 5 };
 
-            sportEvent1 = new SportEvent { Id = 0 };
-            sportEvent1.Results = new List<Result>();
+            this.sportEvent1 = new SportEvent { Id = 0 };
+            this.sportEvent1.Results = new List<Result>();
 
-            sportEvent2 = new SportEvent { Id = 1 };
-            sportEvent2.Results = new List<Result>();
+            this.sportEvent2 = new SportEvent { Id = 1 };
+            this.sportEvent2.Results = new List<Result>();
 
-            sportEvent3 = new SportEvent { Id = 2 };
-            sportEvent3.Results = new List<Result>();
+            this.sportEvent3 = new SportEvent { Id = 2 };
+            this.sportEvent3.Results = new List<Result>();
 
             DateTime date = new DateTime(2016, 11, 21, 16, 30, 0);
 
-            sportEvent1.Date = date;
-            sportEvent2.Date = date;
-            sportEvent3.Date = date;
+            this.sportEvent1.Date = date;
+            this.sportEvent2.Date = date;
+            this.sportEvent3.Date = date;
 
-            sportEvent1.SportType = sportType;
-            sportEvent2.SportType = sportType;
-            sportEvent3.SportType = sportType;
+            this.sportEvent1.SportType = this.sportType;
+            this.sportEvent2.SportType = this.sportType;
+            this.sportEvent3.SportType = this.sportType;
 
-            sportEvent1.Results.Add(res1);
-            sportEvent1.Results.Add(res2);
-            sportEvent2.Results.Add(res1);
-            sportEvent2.Results.Add(res2);
+            this.sportEvent1.Results.Add(this.res1);
+            this.sportEvent1.Results.Add(this.res2);
+            this.sportEvent2.Results.Add(this.res1);
+            this.sportEvent2.Results.Add(this.res2);
 
-            sportEvent3.Results.Add(res1);
-            sportEvent3.Results.Add(res3);
+            this.sportEvent3.Results.Add(this.res1);
+            this.sportEvent3.Results.Add(this.res3);
         }
 
         [Test]
@@ -58,78 +58,78 @@ namespace JinnSports.UnitTests.Entities
         {
             Assert.DoesNotThrow(() => 
             {
-                sportEvent = new SportEvent();
-                sportEvent.Results = new List<Result>();
+                this.sportEvent = new SportEvent();
+                this.sportEvent.Results = new List<Result>();
 
-                sportEvent.Id = 0;
-                sportEvent.Date = new DateTime(2016, 11, 21, 16, 30, 0);
-                sportEvent.SportType = sportType;
-                sportEvent.Results.Add(res1);
-                sportEvent.Results.Add(res2);
+                this.sportEvent.Id = 0;
+                this.sportEvent.Date = new DateTime(2016, 11, 21, 16, 30, 0);
+                this.sportEvent.SportType = this.sportType;
+                this.sportEvent.Results.Add(this.res1);
+                this.sportEvent.Results.Add(this.res2);
             });
         }
 
         [Test]
         public void CheckHashIsCorrect()
         {
-            sportEvent = new SportEvent();
-            sportEvent.Results = new List<Result>();
+            this.sportEvent = new SportEvent();
+            this.sportEvent.Results = new List<Result>();
 
             DateTime date = new DateTime(2016, 11, 21, 16, 30, 0);
 
-            sportEvent.Date = date;
-            sportEvent.SportType = sportType;
-            sportEvent.Results.Add(res1);
-            sportEvent.Results.Add(res2);
+            this.sportEvent.Date = date;
+            this.sportEvent.SportType = this.sportType;
+            this.sportEvent.Results.Add(this.res1);
+            this.sportEvent.Results.Add(this.res2);
 
-            int hashCheck = date.GetHashCode() ^ sportType.Name.GetHashCode() ^ res1.Team.Name.GetHashCode() ^ res2.Team.Name.GetHashCode();
-            int hashCheckReverce = date.GetHashCode() ^ sportType.Name.GetHashCode() ^ res2.Team.Name.GetHashCode() ^ res1.Team.Name.GetHashCode();
+            int hashCheck = date.GetHashCode() ^ this.sportType.Name.GetHashCode() ^ this.res1.Team.Name.GetHashCode() ^ this.res2.Team.Name.GetHashCode();
+            int hashCheckReverce = date.GetHashCode() ^ this.sportType.Name.GetHashCode() ^ this.res2.Team.Name.GetHashCode() ^ this.res1.Team.Name.GetHashCode();
 
-            Assert.IsTrue(hashCheck == sportEvent.GetHashCode() && hashCheckReverce == sportEvent.GetHashCode());
+            Assert.IsTrue(hashCheck == this.sportEvent.GetHashCode() && hashCheckReverce == this.sportEvent.GetHashCode());
         }
 
         [Test]
         public void CheckHashCodeSameInstances()
         {
-            Assert.IsTrue(sportEvent1.GetHashCode() == sportEvent2.GetHashCode());
+            Assert.IsTrue(this.sportEvent1.GetHashCode() == this.sportEvent2.GetHashCode());
         }
 
         [Test]
         public void CheckHashCodeNotSameInstances()
         {
-            Assert.IsFalse(sportEvent1.GetHashCode() == sportEvent3.GetHashCode());
+            Assert.IsFalse(this.sportEvent1.GetHashCode() == this.sportEvent3.GetHashCode());
         }
 
         [Test]
         public void CheckEquals()
         {
-            Assert.IsTrue(sportEvent1.Equals(sportEvent2) && sportEvent2.Equals(sportEvent1));
+            Assert.IsTrue(this.sportEvent1.Equals(this.sportEvent2) && this.sportEvent2.Equals(this.sportEvent1));
         }
 
         [Test]
         public void CheckNotEquals()
         {
-            Assert.IsFalse(sportEvent1.Equals(sportEvent3) && sportEvent3.Equals(sportEvent1));
+            Assert.IsFalse(this.sportEvent1.Equals(this.sportEvent3) && this.sportEvent3.Equals(this.sportEvent1));
         }
 
         [Test]
         public void CheckSetContainsDifferent()
         {
             ICollection<SportEvent> set = new HashSet<SportEvent>();
-            set.Add(sportEvent1);
-            set.Add(sportEvent3);
+            set.Add(this.sportEvent1);
+            set.Add(this.sportEvent3);
 
-            Assert.IsTrue(set.Contains(sportEvent1) && set.Contains(sportEvent3) && (set.Count == 2));
+            Assert.IsTrue(set.Contains(this.sportEvent1) && set.Contains(this.sportEvent3) && (set.Count == 2));
         }
 
         [Test]
         public void CheckSetNotContainsSame()
         {
             ICollection<SportEvent> set = new HashSet<SportEvent>();
-            set.Add(sportEvent1);
-            set.Add(sportEvent2);
+            set.Add(this.sportEvent1);
+            set.Add(this.sportEvent2);
             
-            Assert.IsTrue(set.Contains(sportEvent1) && (set.Count == 1));
+            Assert.IsTrue(set.Contains(this.sportEvent1) && (set.Count == 1));
         }
     }
 }
