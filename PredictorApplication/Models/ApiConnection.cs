f28555c48dab090c3614abfd07c7226dd8d79136
ivan-rataichuk <ivan.rataichuk@gmail.com>
2017-01-19 22:43:36 +0200

    Added ScorePredictor Solution

---
+++ PredictorApplication/Models/ApiConnection.cs
@@ -0,0 +1,66 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using PredictorApplication.Models;
+using PredictorDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+using System.Xml;
+
+namespace PredictorApplication.Models
+{
+    public class ApiConnection
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(ApiConnection));
+
+        private string baseUrl;
+        private string controllerUrn;
+        private int timeoutSec;
+
+        public async void SendPredictions(IEnumerable<PredictionDTO> predictions)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    Log.Info("Starting Data transfer");
+
+                    GetConnectionSettings();
+
+                    client.BaseAddress = new Uri(baseUrl);
+                    client.Timeout = new TimeSpan(0, 0, timeoutSec);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    string json = JsonConvert.SerializeObject(predictions, Newtonsoft.Json.Formatting.Indented);
+                    StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.PostAsync(controllerUrn, content);
+                    if (response.IsSuccessStatusCode)   
+                    {
+                        Log.Info("Data sucsessfully transfered");
+                    }
+                    else
+                    {
+                        Log.Info("Error occured during Data transfer");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error("Exception occured while trying to send Predictions", ex);
+                }
+            }
+        }
+
+        private void GetConnectionSettings()
+        {
+            baseUrl = PredictorMonitor.GetInstance().CallBackURL;
+            controllerUrn = PredictorMonitor.GetInstance().CallBackController;
+            timeoutSec = PredictorMonitor.GetInstance().CallBackTimeout;
+        }
+    }
+}
---
+++ PredictorApplication/Models/EventData/TeamEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScorePredictor.EventData
+{
+    public class TeamEvent
+    {
+        public int AttackScore { get; set; }
+        public int DefenceScore { get; set; }
+        public bool IsHomeGame { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
